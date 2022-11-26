19d77382db67a74e66e6b064d37e38764eeefcba
agus ufrod <lkelinci0@gmail.com>
2022-11-26 14:53:15 +0700

    update

--- SQL/src/config/mysql.js
+++ SQL/src/config/mysql.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 var Sequelize = require('sequelize');
-const { DB_USERNAME, DB_PASSWORD, DB_HOSTNAME, DB_NAME, DB_DIALECT } = process.env;
-var db = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+const { MYSQLUSER, MYSQLPASSWORD, MYSQLHOST, DB_NAME, DB_DIALECT } = process.env;
+var db = new Sequelize(DB_NAME, MYSQLUSER, MYSQLPASSWORD, {
   dialect: DB_DIALECT,
-  host: DB_HOSTNAME,
+  host: MYSQLHOST,
 });
 
 module.exports = db;
